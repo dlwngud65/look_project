84de5854acd8a7b5e438dcdb32811df8423f410b
dlwngud65 <dlwngud65@daum.net>
2018-07-31 17:24:24 +0900

    관리자 상품 수정 시 이미지가 삭제 안되서 수정 후 다시 올립니다.

--- admin/admin_content_product/delete_img_goods.php
+++ admin/admin_content_product/delete_img_goods.php
@@ -11,7 +11,7 @@ if(!empty($_GET['num'])){
 
 include '../../lib/dbconn.php';
 
-$sql = "select * from goods where goods_num= $num ";
+$sql = "select * from goods where goods_num= '$num' ";
 $result = mysqli_query($con, $sql) or die("실패원인 : " . mysqli_error($con));
 $row = mysqli_fetch_array($result);
 
@@ -27,21 +27,21 @@ $copied_name[8] = $row['file_copied_8'];
 $copied_name[9] = $row['file_copied_9'];
 
 if($delete == 'file0'){
-    $sql = "update goods set file_name_0='',file_copied_0='' where goods_num= $num";
+    $sql = "update goods set file_name_0='',file_copied_0='' where goods_num= '$num'";
     mysqli_query($con, $sql) or die("실패원인 : " . mysqli_error($con));
     if ($copied_name[0]) {
         $image_name = "./data/" . $copied_name[0];
         unlink($image_name);
     }
 } else if($delete == 'file1'){
-    $sql = "update goods set file_name_1='',file_copied_1='' where goods_num= $num";
+    $sql = "update goods set file_name_1='',file_copied_1='' where goods_num= '$num'";
     mysqli_query($con, $sql) or die("실패원인 : " . mysqli_error($con));
     if ($copied_name[1]) {
         $image_name = "./data/" . $copied_name[1];
         unlink($image_name);
     }
 }else if($delete == 'file2'){
-    $sql = "update goods set file_name_2='',file_copied_2='' where goods_num= $num";
+    $sql = "update goods set file_name_2='',file_copied_2='' where goods_num= '$num'";
     mysqli_query($con, $sql) or die("실패원인 : " . mysqli_error($con));
     if ($copied_name[2]) {
         $image_name = "./data/" . $copied_name[2];
@@ -55,42 +55,42 @@ if($delete == 'file0'){
         unlink($image_name);
     }
 }else if($delete == 'file4'){
-    $sql =  "update goods set file_name_4='',file_copied_4='' where goods_num= $num";
+    $sql =  "update goods set file_name_4='',file_copied_4='' where goods_num= '$num'";
     mysqli_query($con, $sql) or die("실패원인 : " . mysqli_error($con));
     if ($copied_name[4]) {
         $image_name = "./data/" . $copied_name[4];
         unlink($image_name);
     }
 }else if($delete == 'file5'){
-    $sql =  "update goods set file_name_5='',file_copied_5='' where goods_num= $num";
+    $sql =  "update goods set file_name_5='',file_copied_5='' where goods_num= '$num'";
     mysqli_query($con, $sql) or die("실패원인 : " . mysqli_error($con));
     if ($copied_name[5]) {
         $image_name = "./data/" . $copied_name[5];
         unlink($image_name);
     }
 }else if($delete == 'file6'){
-    $sql =  "update goods set file_name_6='',file_copied_6='' where goods_num= $num";
+    $sql =  "update goods set file_name_6='',file_copied_6='' where goods_num= '$num'";
     mysqli_query($con, $sql) or die("실패원인 : " . mysqli_error($con));
     if ($copied_name[6]) {
         $image_name = "./data/" . $copied_name[6];
         unlink($image_name);
     }
 }else if($delete == 'file7'){
-    $sql =  "update goods set file_name_7='',file_copied_7='' where goods_num= $num";
+    $sql =  "update goods set file_name_7='',file_copied_7='' where goods_num= '$num'";
     mysqli_query($con, $sql) or die("실패원인 : " . mysqli_error($con));
     if ($copied_name[7]) {
         $image_name = "./data/" . $copied_name[7];
         unlink($image_name);
     }
 }else if($delete == 'file8'){
-    $sql =  "update goods set file_name_8='',file_copied_8='' where goods_num= $num";
+    $sql =  "update goods set file_name_8='',file_copied_8='' where goods_num= '$num'";
     mysqli_query($con, $sql) or die("실패원인 : " . mysqli_error($con));
     if ($copied_name[8]) {
         $image_name = "./data/" . $copied_name[8];
         unlink($image_name);
     }
 }else if($delete == 'file9'){
-    $sql =  "update goods set file_name_9='',file_copied_9='' where goods_num= $num";
+    $sql =  "update goods set file_name_9='',file_copied_9='' where goods_num= '$num'";
     mysqli_query($con, $sql) or die("실패원인 : " . mysqli_error($con));
     if ($copied_name[9]) {
         $image_name = "./data/" . $copied_name[9];
